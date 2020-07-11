5d77ec94cc87f6f8bd51a97c87811cdb89f55d41
Saba Zubair <saba@postageapp.com>
2020-07-11 16:26:48 -0400

    completed Ascending List

---
+++ labs109.py
@@ -0,0 +1,22 @@
+def ryerson_letter_grade(n):
+    if n < 50:
+        return 'F'
+    elif n > 89:
+        return 'A+'
+    elif n > 84:
+        return 'A'
+    elif n > 79:
+        return 'A-'
+    tens = n // 10
+    ones = n % 10
+    if ones < 3:
+        adjust = "-"
+    elif ones > 6:
+        adjust = "+"
+    else:
+        adjust = ""
+    return "DCB"[tens - 5] + adjust
+
+
+def is_ascending(items):
+    return all(i < j for i, j in zip(items, items[1:]))
