d891b7dedb3f251635c9f8bdb09e29d71abadce9
Saba Zubair <zubairs19@protonmail.com>
2020-07-16 16:50:51 -0400

    partial solution to only_odd_digits, discrepancy at test case #7

--- labs109.py
+++ labs109.py
@@ -17,6 +17,20 @@ def ryerson_letter_grade(n):
         adjust = ""
     return "DCB"[tens - 5] + adjust
 
+# PASSED
+
 
 def is_ascending(items):
     return all(i < j for i, j in zip(items, items[1:]))
+
+# Failing
+
+
+def only_odd_digits(n):
+    for n in str(n):
+        # print(type(n))
+        # print(n)
+        if n == "1" or n == "5" or n == "7" or n == "9":
+            return True
+        else:
+            return False
