b2c238e0aecc0afa209dd287716c2c1c95a5b915
Saba Zubair <zubairs19@protonmail.com>
2020-07-30 17:13:53 -0400

    completed nearest_smaller problem

--- labs109.py
+++ labs109.py
@@ -42,6 +42,21 @@ def knight_jump(steps, start, end):
     return steps == tuple(sorted((abs(x - y) for (x, y) in zip(start, end)), reverse=True))
 
 
+def nearest_smaller(items):
+    n, result = len(items), []
+    for (i, e) in enumerate(items):
+        j = 1
+        while j < n:
+            left = items[i - j] if i >= j else e
+            right = items[i + j] if i+j < n else e
+            if left < e or right < e:
+                result.append(left if left < right else right)
+                break
+            j += 1
+        else:
+            result.append(e)
+    return result
+
 # # 2. Failing, Test #17
 
 
